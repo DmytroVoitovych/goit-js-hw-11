5462b9b5f6699c6f404e64cd71c2b0c4ad510fd6
DmytroVoitovych <begunec23@gmail.com>
2022-07-16 14:02:04 +0200

    test finish

--- src/index.js
+++ src/index.js
@@ -1,134 +1,59 @@
-const axios = require('axios').default;
+import { response } from './js/fetch';
+import { getUser } from './js/fetch';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// Описан в документации
-import SimpleLightbox from "simplelightbox";
-// Дополнительный импорт стилей
-import "simplelightbox/dist/simple-lightbox.min.css";
 var throttle = require('lodash.throttle');
 
+let page = 1;
+
 const form = document.querySelector('#search-form');
-const searchInput = document.querySelector('[name="searchQuery"]');
 const gallery = document.querySelector(".gallery");
 
-let page = 1;
-let response = null; 
-let val = null;
-
-const optionUrl = {
-    key: '28460134-7b12f16a69bff4fc5524ed994',
-    findTermin: searchInput.value.trim(),
-    imgType: 'photo',
-    orientation: 'horisontal',
-    ageFilterDate: true,
-    pagePer: 40,
+export const moveScrolle = throttle(() => {
+  if (gallery.children.length != 0) {
+    const seeLastElement = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        const currentEntry = entry.target;
+        if (!entry.isIntersecting) {
+          return;
+        }
+      
+        else if (gallery.children.length >= response.data.totalHits) {
+          Notify.info(`We're sorry, but you've reached the end of search results.`);
+          seeLastElement.unobserve(currentEntry);
+        }
+        else {
+          page+=1;
+          getUser();
+          seeLastElement.unobserve(currentEntry);
+        }
+      });
+    }, { threshold: 0.25, root: null, rootMargin: '0px', });
+  
+    seeLastElement.observe(document.querySelector(".grid-item:last-child"));
   }
-
-const moveScrolle = throttle(() => {
-  const d = document.documentElement.getBoundingClientRect();
-  if (d.bottom < document.documentElement.clientHeight + 450) {
-    console.log('done');
-    page++;
-    getUser();
-    const { height: cardHeight } = document
-      .querySelector(".photo-card")
-      .firstElementChild.getBoundingClientRect();
-
-    window.scrollBy({
-      top: cardHeight * 2,
-      behavior: "smooth",
-    });
-  }
+    
 }, 300);
 
-var lightbox = new SimpleLightbox('.gallery__link', { captionDelay: 250, captionClass: 'test' });   
+  
 
-form.addEventListener('submit',  (e) => {
-    window.addEventListener('scroll', moveScrolle);
+form.addEventListener('submit',  async (e) => {
+    
     e.preventDefault();
     gallery.innerHTML = '';
-    page = 1;
- if (gallery.children.length < 40) {
-       getUser();
-  
-     
-      } 
+  page = 1;
+  await getUser();    
+  if (gallery.children.length == 0) {
+         await  Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+        }
 });
 
+export { page };
 
 
-const renderImg = (findedImg) => {
-const imgGallery = findedImg.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
-return `<div class="grid-item" ><a  class="gallery__link" href="${largeImageURL}">
-    <div class="photo-card">
-  <img  class="gallery__image"
-      src="${webformatURL}"
-      data-source="${largeImageURL}"
-      alt="'${tags}'"
-      title='${tags}'
-       loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes: ${likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views: ${views} </b>
-    </p>
-    <p class="info-item">
-      <b>Comments: ${comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads: ${downloads}</b>
-    </p>
-  </div>
-</div>
-  </a>
-  </div>`;
-}).join(" ");
-    gallery.insertAdjacentHTML("beforeend", ` ${imgGallery}`);  
-    lightbox.refresh();
-    
-};
 
-searchInput.addEventListener('input', () => val = searchInput.value.trim());
 
-async function getUser() {
-    try {
-     
-     response = await axios.get(`https://pixabay.com/api/?key=${optionUrl.key}&q=${val}&image_type=${optionUrl.imgType}&orientation=${optionUrl.orientation}&safesearch=${optionUrl.ageFilterDate}&per_page=${optionUrl.pagePer}&page=${page}`);
-       console.log(searchInput.value.trim()); 
-      const render = await renderImg(response.data.hits);
-                  if (gallery.children.length >= response.data.totalHits) {
-                 Notify.info(`We're sorry, but you've reached the end of search results.`);
-                 window.removeEventListener('scroll', moveScrolle);
-                  }
-                  else if (gallery.children.length <= 40) {
-            Notify.info(`Hooray! We found ${response.data.totalHits} images.`);        
-      }
-         else if (response.data.total === 0 ) {
-           throw ('Sorry, there are no images matching your search query. Please try again.');
-        }
-          
-        console.log(response);
-         } catch (error) {
-        
-        Notify.failure(`${error}`);
-  }
-};
 
-//
 
-// const getValue = (e) => {
-//    optionUrl = {
-//     key: '28460134-7b12f16a69bff4fc5524ed994',
-//     findTermin: searchInput.value.trim(),
-//     imgType: 'photo',
-//     orientation: 'horisontal',
-//     ageFilterDate: true,
-//     pagePer: 40,
-//   }
-
-//   return optionUrl;
-// };
 
 
   
---
+++ src/js/option.js
@@ -0,0 +1,10 @@
+
+
+export const optionUrl = {
+    key: '28460134-7b12f16a69bff4fc5524ed994',
+    // findTermin: searchInput.value.trim(),
+    imgType: 'photo',
+    orientation: 'horisontal',
+    ageFilterDate: true,
+    pagePer: 40,
+};
